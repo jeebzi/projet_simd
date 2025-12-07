e7696492281f54b3b3706699c5a4ac0ef02f388f
NGUYEN JEAN-BAPTISTE <jeannguyen033@gmail.com>
2025-12-07 10:52:20 +0100

    affiche vecteur, matrice to vecteur

--- makefile
+++ makefile
@@ -3,3 +3,6 @@ main.exe : main.c count.h
 
 prod_matrice_simd.exe : prod_matrice_simd.c
 	gcc -mavx512f -Wall -g -o prod_matrice_simd.exe prod_matrice_simd.c
+
+clean:
+	rm *.exe
--- prod_matrice_simd.c
+++ prod_matrice_simd.c
@@ -10,38 +10,52 @@ __m512i* matrice_into_vecteur(int32_t *matrice, int n, int m) {
 	__m512i *vecteur, m512;
 	vecteur = (__m512i*) malloc(64 * ((n * m) / 8));
 
-	int i = 0, j;
+	int i = 0, j, indice_vecteur=0;
 	while (i < n) {
 		j = 0;
 		while ((m - j) >= 8) {
 			m512 = _mm512_set_epi64(matrice[i*m+(j + 7)], matrice[i*m+(j + 6)], matrice[i*m+(j + 5)], matrice[i*m+(j + 4)], matrice[i*m+(j + 3)], matrice[i*m+(j + 2)], matrice[i*m+(j + 1)], matrice[i*m+(j + 0)]);
-			_mm512_storeu_epi64((void*)&vecteur[i+j/8], m512);
+			_mm512_storeu_epi64((void*)&vecteur[indice_vecteur], m512);
 			//vecteur[i + j] = m512;
 			j += 8;
+			indice_vecteur += 1;
 		}
 		if (m != j) { // il reste des valeur dans la lignes de ma matrice en rentrer dans un vecteur
 			int64_t *reste;
-			reste = (int64_t*) calloc(6, 8);
-			int ecart = m - j;
-			while (ecart < m) {
-				reste[m - (ecart - (m - j) + 1)] = matrice[i*m + j];
-				ecart += 1;
+			reste = (int64_t*) calloc(8, 8);
+			int ecart = m - j, k=0;
+			while (k<ecart) {
+				reste[k] = matrice[i*m + j];
+				j += 1;
+				k += 1;
 			}
+			m512 = _mm512_loadu_epi64(reste);
+			_mm512_storeu_epi64((void*)&vecteur[indice_vecteur], m512);
+			indice_vecteur += 1;
 		}
 		i += 1;
 	}
 	return vecteur;
 }
 
+void affiche_vecteur(__m512i *vecteur, int n, int m) {
+	/* affiche sur le terminal les valeurs de la matrice n*m représenté dans le vecteur de m512
+	 */
+	for (int i = 0; i < n*((m+7)/8); i++) {
+		for (int j=0; j < 8; j++)
+		printf("%lld ", vecteur[i][j]);
+		printf("\n");
+	}
+}
+
+
 int main() {
 	int32_t *matrice;
 	int n = 8, m = 8;
 	matrice = (int32_t*) calloc(n*m, sizeof(int32_t));
-	matrice[3] = 1;
+	matrice[8] = 1;
 	__m512i* vecteur;
 	vecteur = matrice_into_vecteur(matrice, n, m);
-	for (int i = 0; i < n*m; i++) {
-		printf("%lld\n", vecteur[i/n][i%n]);
-	}
+	affiche_vecteur(vecteur , n, m);
 	return 0;
 }
